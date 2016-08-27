b490f6a8e0d6126565e436ad9152f3aba986be24
赵东亮 <3012287667@qq.com>
2016-08-27 15:04:55 +0800

    2. 删除系统的类及storyboard

---
+++ NavAlpha/ZDNavTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  ZDNavTableViewController.h
+//  NavAlpha
+//
+//  Created by 赵东亮 on 16/8/27.
+//  Copyright © 2016年 赵东亮. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZDNavTableViewController : UITableViewController
+
+@end
